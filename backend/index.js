5bcdfdaa30b3e75c01bff7b0756a204e677e9a6f
PloyIIV <chadapan.y58@gmail.com>
2024-05-21 21:44:09 +0700

    backend commit

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,27 @@
 import bodyParser from "body-parser";
 import express from "express";
-import cors from 'cors'
+import cors from "cors";
 
-const app = express();
-const port = 4000;
+async function init() {
+  const app = express();
+  const port = 4000;
 
-app.use(cors());
-app.use(bodyParser.json());
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+  app.use(cors());
+  app.use(bodyParser.json());
+  // app.use(express.json());
+  // app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send("API is working")
-})
+  app.get("/", (req, res) => {
+    res.send("API is working");
+  });
 
-app.get('*', (req, res) => {
-    res.status(404).send("ERROR: 404 NOT FOUND")
-})
+  app.get("*", (req, res) => {
+    res.status(404).send("ERROR: Server");
+  });
 
-app.listen(port, () => {
-    console.log(`Server is running at port: ${port}`)
-})
+  app.listen(port, () => {
+    console.log(`Server is running at port: ${port}`);
+  });
+}
 
-export default app
+init();
