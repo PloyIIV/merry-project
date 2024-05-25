2b2f8a7958c1ceffd7790e35103d55078033517a
PloyIIV <chadapan.y58@gmail.com>
2024-05-25 23:25:10 +0700

    feat: add login feature on backend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { supabase } from "../config/db.js";
+import jwt from 'jsonwebtoken'
 
 const userRouter = express.Router();
 
+// TEST API
 userRouter.get('/log', async (req, res) => {
     try {
         const { data, error } = await supabase.from('users').select();
@@ -20,7 +22,43 @@ userRouter.get('/log', async (req, res) => {
 })
 
 userRouter.post('/login', async (req, res) => {
-    const { username, password } = req.body
+    const { password, username } = req.body;
+    let lowerCaseEmail;
+    if (req.body.email) {
+        lowerCaseEmail = req.body.email.toLowerCase();
+    }
+    try {
+        const { data, error } = await supabase.from('users').select('*').or(`email.eq.${lowerCaseEmail}, username.eq.${username}`);
+        if (error) {
+            return res.json({
+                message: error
+            })
+        }
+        if (!data[0]) {
+            return res.status(404).json({
+                message: `This email is not exists, Please sign up first.`
+            })
+        }
+        if (password !== data[0].password) {
+            return res.json({
+                message: `Incorrect password. If you can't remember, Please reset password.`
+            })
+        }
+        const token = jwt.sign(
+            { id: data[0].id },
+            process.env.SUPABASE_JWT,
+            { expiresIn: "900000" }
+        )
+        return res.json({
+            data: data[0],
+            token
+        })
+    } catch (error) {
+        console.error(error);
+        res.json({
+            message: `ERROR: There's something wrong, Can't login right now. ${error}`
+        })
+    }
 })
 
 userRouter.post('/register', async (req, res) => {
