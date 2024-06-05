8f6ef1369b88a528083d8444033149385bab1149
PloyIIV <chadapan.y58@gmail.com>
2024-06-06 02:21:47 +0700

    style: style button on homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 const Home = () => {
   const  navigate = useNavigate();
   return (
-    <div className="bg-putility-400 min-h-dvh">
+    <div className="bg-putility-400 min-h-dvh flex justify-center">
       <div className="w-96 text-center text-white">
         <h1 className="font-black text-6xl">Make the first ‘Merry’</h1>
         <div className="font-semibold text-xl mt-10 mb-14">
@@ -13,7 +13,7 @@ const Home = () => {
             <p>new people in your area!</p>
             <p>Dont’t forget to get Merry with us</p>
         </div>
-        <button onClick={() => navigate('/matching')} className='bg-pred-500 text-white font-bold rounded-full w-24 h-12'>Start matching!</button>
+        <button onClick={() => navigate('/matching')} className='bg-pred-500 text-white font-bold rounded-full w-40 h-12'>Start matching!</button>
       </div>
       <div id="why-merry"></div>
       <div id="how-to"></div>
