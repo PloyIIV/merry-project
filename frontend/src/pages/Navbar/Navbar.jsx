e7e275bdd9d02eb153af0d6a4fe7a070b4a05c2c
PloyIIV <chadapan_ployza@hotmail.com>
2025-06-03 16:37:48 +0700

    style: add noti button

--- frontend/src/pages/Navbar/Navbar.jsx
+++ frontend/src/pages/Navbar/Navbar.jsx
@@ -123,6 +123,7 @@ const Navbar = () => {
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(null);
   const [clicked, setClicked] = useState(false);
+  const [notiClicked, setNotiClicked] = useState(false);
   const [user, setUser] = useState("");
   const fetchData = async () => {
     setIsLoading(true);
@@ -138,36 +139,118 @@ const Navbar = () => {
   return (
     <div className="flex justify-around h-20 items-center shadow-md relative z-10">
       {clicked ? (
-        <div onMouseLeave={() => setClicked(!clicked)} className="z-50 flex flex-col w-[230px] h-[250px] p-3 bg-white text-pgray-700 rounded-xl shadow-2xl border border-pgray-400 shadow-gray-600 absolute right-[14%] top-16">
+        <div
+          onMouseLeave={() => setClicked(!clicked)}
+          className="z-50 flex flex-col w-[230px] h-[250px] p-3 bg-white text-pgray-700 rounded-xl shadow-2xl border border-pgray-400 shadow-gray-600 absolute right-[14%] top-16"
+        >
           <button className="flex items-center justify-center text-sm font-semibold text-white rounded-full py-2 px-4 mb-2 bg-gradient-to-r from-pred-700 to-ppurple-300">
             {star}More limit Merry!
           </button>
           <div className="text-sm">
-            <div onClick={()=>{ navigate('/profile') }} className="flex py-2 hover:bg-pgray-200 hover:rounded-xl cursor-pointer">
+            <div
+              onClick={() => {
+                navigate("/profile");
+              }}
+              className="flex py-2 hover:bg-pgray-200 hover:rounded-xl cursor-pointer"
+            >
               <div className="ml-2">{icon1}</div>
               <p className="ml-3">Profile</p>
             </div>
-            
+
             <div className="flex py-2 hover:bg-pgray-200 hover:rounded-xl cursor-pointer">
               <div className="ml-2">{icon2}</div>
               <p className="ml-3">Merry list</p>
             </div>
-            
+
             <div className="flex py-2 hover:bg-pgray-200 hover:rounded-xl cursor-pointer">
               <div className="ml-2">{icon3}</div>
               <p className="ml-3">Merry Membership</p>
             </div>
-            
-            <div onClick={() => navigate('/complain')} className="flex py-2 hover:bg-pgray-200 hover:rounded-xl cursor-pointer">
+
+            <div
+              onClick={() => navigate("/complain")}
+              className="flex py-2 hover:bg-pgray-200 hover:rounded-xl cursor-pointer"
+            >
               <div className="ml-2">{icon4}</div>
               <p className="ml-3">Complaint</p>
             </div>
           </div>
           <hr className="mt-1" />
-          <div onClick={() => logout()} className="flex py-2 mt-1 items-center cursor-pointer hover:bg-pgray-200 hover:rounded-xl">
-              <div className="ml-2">{logoutIcon}</div>
-              <p className="ml-3 text-sm">Logout</p>
+          <div
+            onClick={() => logout()}
+            className="flex py-2 mt-1 items-center cursor-pointer hover:bg-pgray-200 hover:rounded-xl"
+          >
+            <div className="ml-2">{logoutIcon}</div>
+            <p className="ml-3 text-sm">Logout</p>
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+      {notiClicked ? (
+        <div
+          onMouseLeave={() => setNotiClicked(!notiClicked)}
+          className="z-50 flex flex-col w-[15%] p-2 bg-white text-pgray-700 rounded-xl shadow-2xl border border-pgray-400 shadow-gray-600 absolute right-[14%] top-16"
+        >
+          <div className="rounded-md p-2 flex hover:bg-pgray-200 mb-1">
+            <div className="relative">
+              <svg
+                className="absolute bottom-1 right-0"
+                width="15"
+                height="15"
+                viewBox="0 0 10 10"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M3.96689 8.53302C4.16585 8.65864 4.36884 8.77777 4.57554 8.89023L4.57552 8.89027L4.57874 8.89197L4.58575 8.89618L4.60029 8.90344C4.72405 8.96741 4.86141 9.00072 5.00079 9.00052C5.09238 9.00038 5.18306 8.98578 5.26954 8.95753H5.312L5.42101 8.89212L5.4254 8.88972C6.12247 8.50853 6.77826 8.05627 7.3823 7.54017L7.3823 7.54017L7.38396 7.53874C8.3233 6.729 9.5 5.40955 9.5 3.75003V3.74998C9.49995 3.18132 9.3236 2.62666 8.99525 2.16237C8.6669 1.69808 8.20269 1.347 7.66654 1.15748C7.13039 0.967947 6.54867 0.94929 6.00148 1.10407C5.63298 1.20831 5.29235 1.38775 5 1.62892C4.70766 1.38775 4.36702 1.20831 3.99852 1.10407C3.45133 0.94929 2.86961 0.967947 2.33346 1.15748C1.79731 1.347 1.3331 1.69808 1.00475 2.16237C0.676397 2.62666 0.500054 3.18132 0.5 3.74998V3.75003C0.5 5.40959 1.67729 6.72907 2.61594 7.53866L2.61766 7.54013C3.04272 7.90339 3.49365 8.23523 3.96689 8.53302ZM3.96689 8.53302C3.96677 8.53295 3.96666 8.53288 3.96655 8.53281L4.2335 8.11003L3.96722 8.53323C3.96711 8.53316 3.967 8.53309 3.96689 8.53302Z"
+                  fill="#FF1659"
+                  stroke="white"
+                />
+              </svg>
+              <img
+                className="w-10 h-10 object-cover rounded-full"
+                src="https://parade.com/.image/t_share/MTk0OTMzNjkzNDM1ODgwOTg0/gettyimages-1445629372.jpg"
+                alt="sadies"
+              />
             </div>
+            <div className="ml-3">
+              <p className="text-sm">
+                '<span className="font-semibold">Sadies Sink</span>' Just Merry
+                you!
+              </p>
+              <p className="text-[13px]">Click here to see profile</p>
+            </div>
+          </div>
+          <div className="rounded-md p-2 flex hover:bg-pgray-200">
+            <div className="relative">
+              <svg
+                className="absolute bottom-1 right-0"
+                width="15"
+                height="15"
+                viewBox="0 0 10 10"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M3.96689 8.53302C4.16585 8.65864 4.36884 8.77777 4.57554 8.89023L4.57552 8.89027L4.57874 8.89197L4.58575 8.89618L4.60029 8.90344C4.72405 8.96741 4.86141 9.00072 5.00079 9.00052C5.09238 9.00038 5.18306 8.98578 5.26954 8.95753H5.312L5.42101 8.89212L5.4254 8.88972C6.12247 8.50853 6.77826 8.05627 7.3823 7.54017L7.3823 7.54017L7.38396 7.53874C8.3233 6.729 9.5 5.40955 9.5 3.75003V3.74998C9.49995 3.18132 9.3236 2.62666 8.99525 2.16237C8.6669 1.69808 8.20269 1.347 7.66654 1.15748C7.13039 0.967947 6.54867 0.94929 6.00148 1.10407C5.63298 1.20831 5.29235 1.38775 5 1.62892C4.70766 1.38775 4.36702 1.20831 3.99852 1.10407C3.45133 0.94929 2.86961 0.967947 2.33346 1.15748C1.79731 1.347 1.3331 1.69808 1.00475 2.16237C0.676397 2.62666 0.500054 3.18132 0.5 3.74998V3.75003C0.5 5.40959 1.67729 6.72907 2.61594 7.53866L2.61766 7.54013C3.04272 7.90339 3.49365 8.23523 3.96689 8.53302ZM3.96689 8.53302C3.96677 8.53295 3.96666 8.53288 3.96655 8.53281L4.2335 8.11003L3.96722 8.53323C3.96711 8.53316 3.967 8.53309 3.96689 8.53302Z"
+                  fill="#FF1659"
+                  stroke="white"
+                />
+              </svg>
+              <img
+                className="w-10 h-10 object-cover rounded-full"
+                src="https://cdni-hw.ch7.com/dm/sz-md/i/images/2024/01/04/659663ab96a002.36484583.jpg"
+                alt="sadies"
+              />
+            </div>
+            <div className="ml-2">
+              <p className="text-sm">
+                '<span className="font-semibold">Aum</span>' Merry you back!
+              </p>
+              <p className="text-[13px]">Let's start conversation now</p>
+            </div>
+          </div>
         </div>
       ) : (
         <></>
@@ -175,7 +258,10 @@ const Navbar = () => {
       <Logo />
       <div className="flex items-center">
         <div>
-          <button onClick={() => navigate('/matching')} className="font-bold text-putility-600">
+          <button
+            onClick={() => navigate("/matching")}
+            className="font-bold text-putility-600"
+          >
             Start Matching!
           </button>
           <button className="font-bold text-putility-600 px-8">
@@ -183,11 +269,17 @@ const Navbar = () => {
           </button>
         </div>
         <div>
-          <button>{notiButton}</button>
+          <button onClick={() => setNotiClicked(!notiClicked)}>
+            {notiButton}
+          </button>
           <button onClick={() => setClicked(!clicked)}>
             {!isLoading ? (
               <div className="ml-2">
-                <img className="object-cover rounded-full w-[48px] h-[48px]" src={user} alt={user} />
+                <img
+                  className="object-cover rounded-full w-[48px] h-[48px]"
+                  src={user}
+                  alt={user}
+                />
               </div>
             ) : (
               <div className="spinner"></div>
